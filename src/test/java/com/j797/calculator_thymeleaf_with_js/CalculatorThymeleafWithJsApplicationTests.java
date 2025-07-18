1194f54f9fac05a2a386b2bc82fa761a05c1e224
juyoung <juyoungjeong.797@gmail.com>
2025-07-18 13:45:25 +0900

    Initial commit

---
+++ src/test/java/com/j797/calculator_thymeleaf_with_js/CalculatorThymeleafWithJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j797.calculator_thymeleaf_with_js;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorThymeleafWithJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
